6c7f1de8917ad3dbd4436d9efe5ff5acc46f49e4
Victor Hugo <victor-f250@hotmail.com>
2022-02-16 00:09:02 -0300

    banco

--- springboot-loja/src/main/webapp/loja/src/app/app.module.ts
+++ springboot-loja/src/main/webapp/loja/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoaComponent } from './pessoa/pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PessoaComponent
   ],
   imports: [
     BrowserModule,
---
+++ springboot-loja/src/main/webapp/loja/src/app/pessoa/pessoa.interface.ts
@@ -0,0 +1,5 @@
+export interface Pessoa{
+    nome: string;
+    status: boolean;
+    telefone: number;
+} 
